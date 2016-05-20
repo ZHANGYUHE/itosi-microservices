63038649d5fa8124616bbfbd367c6ef992f092e4
Lei Zhang <coolbeevip@gmail.com>
2016-05-20 12:02:13 +0800

    支持1.4.0

--- support/admin-dashboard/src/main/java/org/iplatform/microservices/support/admindashboard/MonitorDashboardApplication.java
+++ support/admin-dashboard/src/main/java/org/iplatform/microservices/support/admindashboard/MonitorDashboardApplication.java
@@ -1,14 +1,16 @@
 package org.iplatform.microservices.support.admindashboard;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
-@SpringBootApplication
-@EnableAdminServer
+@Configuration
+@EnableAutoConfiguration
 @EnableDiscoveryClient
+@EnableAdminServer
 public class MonitorDashboardApplication {
 
 	public static void main(String[] args) {
