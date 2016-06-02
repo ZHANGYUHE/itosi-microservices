ba1f8e62810319a22f1a5bca758e888857cc9a73
Lei Zhang <coolbeevip@gmail.com>
2016-06-02 08:36:57 +0800

    支持认证服务器注册到Eureka

--- support/auth-server/src/main/java/org/iplatform/microservices/support/authserver/SpringBootOauth2Application.java
+++ support/auth-server/src/main/java/org/iplatform/microservices/support/authserver/SpringBootOauth2Application.java
@@ -4,11 +4,13 @@ import java.security.Principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@EnableEurekaClient
 @Controller
 public class SpringBootOauth2Application {
 	
