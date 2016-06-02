934934a96d902c096b633e9e2545aa8ccf870516
Lei Zhang <coolbeevip@gmail.com>
2016-06-02 18:21:16 +0800

    增加login测试页面

--- core/document-service/src/main/java/org/iplatform/microservices/core/documentservice/config/SecurityConfiguration.java
+++ core/document-service/src/main/java/org/iplatform/microservices/core/documentservice/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
 @EnableWebSecurity
@@ -11,7 +12,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/h2console/**");
+		web.ignoring().antMatchers("/h2console/**").antMatchers("/login").antMatchers("/*.json");
 	}
 
 }
