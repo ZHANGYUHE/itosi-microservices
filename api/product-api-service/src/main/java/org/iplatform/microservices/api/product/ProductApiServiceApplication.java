37aa578466d63689060bda21635186cf9e1188f4
Lei Zhang <coolbeevip@gmail.com>
2016-06-03 16:22:27 +0800

    开发api代理服务

--- api/product-api-service/src/main/java/org/iplatform/microservices/api/product/ProductApiServiceApplication.java
+++ api/product-api-service/src/main/java/org/iplatform/microservices/api/product/ProductApiServiceApplication.java
@@ -1,12 +1,52 @@
 package org.iplatform.microservices.api.product;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import com.netflix.discovery.DiscoveryManager;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableResourceServer
 public class ProductApiServiceApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ProductApiServiceApplication.class);
+
+    static {
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, sslSession) -> true);
+    }
+
+    @Value("${app.rabbitmq.host:localhost}")
+    String rabbitMqHost;
+    
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        LOG.info("Create RabbitMqCF for host: {}", rabbitMqHost);
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitMqHost);
+        return connectionFactory;
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApiServiceApplication.class, args);
+        LOG.info("Register ShutdownHook");
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                LOG.info("Shutting down, unregister from Eureka!");
+                DiscoveryManager.getInstance().shutdownComponent();
+            }
+        });		
 	}
 }
--- composite/product-composite-service/src/main/java/org/iplatform/microservices/composite/product/ProductCompositeServiceApplication.java
+++ composite/product-composite-service/src/main/java/org/iplatform/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,12 +1,39 @@
 package org.iplatform.microservices.composite.product;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import com.netflix.discovery.DiscoveryManager;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableResourceServer
 public class ProductCompositeServiceApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeServiceApplication.class);
 
+    static {
+        // for localhost testing only
+        LOG.warn("Will now disable hostname check in SSL, only to be used during development");
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, sslSession) -> true);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
+		
+        LOG.info("Register ShutdownHook");
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                LOG.info("Shutting down, unregister from Eureka!");
+                DiscoveryManager.getInstance().shutdownComponent();
+            }
+        });		
 	}
 }
