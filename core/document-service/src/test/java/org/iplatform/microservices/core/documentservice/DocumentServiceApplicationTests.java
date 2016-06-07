cb86075b9be5b75bf7224dc461b21c7cb5ccd67d
Lei Zhang <coolbeevip@gmail.com>
2016-06-07 22:03:48 +0800

    删除无效的单元测试注释

--- core/document-service/src/test/java/org/iplatform/microservices/core/documentservice/DocumentServiceApplicationTests.java
+++ core/document-service/src/test/java/org/iplatform/microservices/core/documentservice/DocumentServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package org.iplatform.microservices.core.documentservice;
 
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class DocumentServiceApplicationTests {
 
 	@Test
